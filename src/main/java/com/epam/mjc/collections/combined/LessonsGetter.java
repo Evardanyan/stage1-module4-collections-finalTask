acb45007254fb4f012dc84ccb4c78e512c654cd2
Edgar Vardanyan <e.vardan4an@gmail.com>
2022-09-27 01:31:54 +0400

    first solution

--- src/main/java/com/epam/mjc/collections/combined/LessonsGetter.java
+++ src/main/java/com/epam/mjc/collections/combined/LessonsGetter.java
@@ -1,10 +1,15 @@
 package com.epam.mjc.collections.combined;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class LessonsGetter {
     public Set<String> getLessons(Map<String, List<String>> timetable) {
+        Set<String> result = new HashSet<>();
+        for (Map.Entry<String, List<String>> me : timetable.entrySet()) {
+            for (String x : me.getValue()) {
+                result.add(x);
+            }
+        }
+        return result;
     }
 }
--- src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
+++ src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
@@ -1,9 +1,20 @@
 package com.epam.mjc.collections.combined;
 
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
 public class MapFromKeysCreator {
     public Map<Integer, Set<String>> createMap(Map<String, Integer> sourceMap) {
+        Map<Integer, Set<String>> result = new HashMap<>();
+
+        for (Map.Entry<String, Integer> me :
+                sourceMap.entrySet()) {
+            Set<String> vs = result.getOrDefault(me.getKey().length(), new HashSet<>());
+            vs.add(me.getKey());
+            result.put(me.getKey().length(), vs);
+        }
+        return result;
     }
 }
